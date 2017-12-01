ae036a4f15b6a88146f48dfd78302de408a6912d
Silvio Netto <silvio.netto@gmail.com>
2017-12-01 00:10:03 +0000

    Initial Commit

---
+++ lab1/first.py
@@ -0,0 +1,19 @@
+l = [1,2,3]
+print(l)
+print(len(l))
+print(l.index(1))
+
+my_dict = {'k1':'First','k2':'Secocnd'}
+print(my_dict['k1'])
+print(my_dict.keys())
+print(my_dict.values())
+
+d = {}
+d['animal'] = 'Dog'
+print(d['animal'])
+
+d = {'k1':{'nestkey':{'subnestkey':'value'}}}
+print(d['k1']['nestkey']['subnestkey'])
+print(d.keys())
+print(d.values())
+print(len(d))
