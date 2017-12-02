7b50fa45191f6e2659d2277adf38c7fe5140e24c
Silvio Netto <silvio.netto@gmail.com>
2017-12-02 15:43:30 +0000

    Comprehensions

--- lab3/comprehensions.py
+++ lab3/comprehensions.py
@@ -3,3 +3,11 @@ print(l)
 
 l = [number for number in range(11) if number % 2 == 0]
 print(l)
+
+celsius = [0,10,20,30,40]
+fahrenheit = [ (temp * (9/5.0) + 32) for temp in celsius]
+print(fahrenheit)
+
+# final result is x**4 for num in range(11)
+l = [x**2 for x in [x**2 for x in range(11)]]
+print(l)
